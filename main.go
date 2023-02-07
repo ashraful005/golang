ea2bff55e485db5e1dfc1e0e2fdac92e5054e2e5
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-02-07 19:21:02 +0600

    formatted string

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main(){
+var name string="Ashraful"
+var age int=22
 fmt.Println("Hello Bangladesh!")
 fmt.Println("GitHub local to remote")
+fmt.Println(name," is ",age," years old.")
 }
