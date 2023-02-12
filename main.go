7598e6f9abc80d1cc8a764a0f4c2743d6ef71300
ashraful005 <alam1809005@stud.kuet.ac.bd>
2023-02-12 16:21:38 +0600

    local variables

--- main.go
+++ main.go
@@ -3,24 +3,15 @@ package main
 import "fmt"
 
 func main(){
-//both gives same output
-fmt.Println("1 + 1 = ",1+1)      //integer
-fmt.Println("1 + 1 = ",1.0+1.0)  //floating number
+//local variable
+var name string = "Ashraful"
 
-//strings
-fmt.Println("Hello World")
-fmt.Println(len("Hello World"))
-fmt.Println("Hello World"[1])
-fmt.Println("Hello " + "World")
+fmt.Println(name)
 
-fmt.Println("Hello" == "World")
-fmt.Println("Hello" == "Hello")
-
-
-//variables vs constants
-var a int = 22
-const b int =22
-var name, city string = "Ashraful", "Cumilla"
-fmt.Println(a)
-fmt.Println(b)
 }
+func f(){
+fmt.Println(name)
+}
+
+
+//It will give an error because f() doesn't have access to "name"
